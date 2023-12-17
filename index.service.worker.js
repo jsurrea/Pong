8d0e2ceca9670973a3b14a610a846a90a3da0b54
jsurrea <68788933+jsurrea@users.noreply.github.com>
2023-12-17 23:16:34 +0000

    Deploying to gh-pages from @ jsurrea/Pong@ffc6a96d9292ca9caa82a144f13fa3b31502af74 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1702850814|4050798";
+const CACHE_VERSION = "1702854980|4611912";
 const CACHE_PREFIX = "Pong-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
